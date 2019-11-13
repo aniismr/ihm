3532063a5ed3e93069b2acf8a8bef241fc3ec402
Anis Mrabet <aniismrabet@gmail.com>
2019-11-13 23:14:56 +0100

    updated cities + jobbers slider

--- view/home.view.php
+++ view/home.view.php
@@ -109,7 +109,7 @@
 </div>    
 
 <!-- Features Cities -->
-<div class="section margin-top-65 margin-bottom-65">
+<div class="section gray margin-top-65 margin-bottom-65 padding-bottom-50 padding-top-50">
 	<div class="container">
 		<div class="row">
 
@@ -119,51 +119,352 @@
 					<h3>Nous sommes partout en Tunisie !</h3>
 				</div>
 			</div>
+                <div class="col-xl-2 col-md-2">
+                    <!-- Photo Box -->
+                    <a href="jobs-list-layout-1.html" class="photo-box" data-background-image="public/images/villes/tunis.jpg">
+                        <div class="photo-box-content">
+                            <h3>Tunis</h3>
+                            <span>376 Jobs</span>
+                        </div>
+                    </a>
+                </div>
 
-			<div class="col-xl-3 col-md-6">
-				<!-- Photo Box -->
-				<a href="jobs-list-layout-1.html" class="photo-box" data-background-image="images/featured-city-01.jpg">
-					<div class="photo-box-content">
-						<h3>San Francisco</h3>
-						<span>376 Jobs</span>
-					</div>
-				</a>
+                <div class="col-xl-2 col-md-6">
+                    <!-- Photo Box -->
+                    <a href="jobs-list-layout-full-page-map.html" class="photo-box" data-background-image="public/images/villes/sfax.jpg">
+                        <div class="photo-box-content">
+                            <h3>Sfax</h3>
+                            <span>645 Jobs</span>
+                        </div>
+                    </a>
+                </div>
+
+                <div class="col-xl-2 col-md-6">
+                    <!-- Photo Box -->
+                    <a href="jobs-grid-layout-full-page.html" class="photo-box" data-background-image="public/images/villes/kasserine.jpg">
+                        <div class="photo-box-content">
+                            <h3>Kasserine</h3>
+                            <span>832 Jobs</span>
+                        </div>
+                    </a>
+                </div>
+
+                <div class="col-xl-2 col-md-6">
+                    <!-- Photo Box -->
+                    <a href="jobs-list-layout-2.html" class="photo-box" data-background-image="public/images/villes/beja.jpg">
+                        <div class="photo-box-content">
+                            <h3>Beja</h3>
+                            <span>513 Jobs</span>
+                        </div>
+                    </a>
+                </div>
+                
+                <div class="col-xl-2 col-md-6">
+                    <!-- Photo Box -->
+                    <a href="jobs-list-layout-2.html" class="photo-box" data-background-image="public/images/villes/mahdia.jpg">
+                        <div class="photo-box-content">
+                            <h3>Mahdia</h3>
+                            <span>513 Jobs</span>
+                        </div>
+                    </a>
+                </div>
+                
+                <div class="col-xl-2 col-md-6">
+                    <!-- Photo Box -->
+                    <a href="jobs-list-layout-2.html" class="photo-box" data-background-image="public/images/villes/sousse.jpg">
+                        <div class="photo-box-content">
+                            <h3>Sousse</h3>
+                            <span>513 Jobs</span>
+                        </div>
+                    </a>
+                </div>
+			<div class="col-xl-12">
+				<div class="font-size-3 font-weight-medium d-block " style="text-decoration: underline;">
+					<a href="villes.html">
+                        Voir toutes les villes
+                    </a>
+				</div>
 			</div>
-			
-			<div class="col-xl-3 col-md-6">
-				<!-- Photo Box -->
-				<a href="jobs-list-layout-full-page-map.html" class="photo-box" data-background-image="images/featured-city-02.jpg">
-					<div class="photo-box-content">
-						<h3>New York</h3>
-						<span>645 Jobs</span>
-					</div>
-				</a>
-			</div>
-			
-			<div class="col-xl-3 col-md-6">
-				<!-- Photo Box -->
-				<a href="jobs-grid-layout-full-page.html" class="photo-box" data-background-image="images/featured-city-03.jpg">
-					<div class="photo-box-content">
-						<h3>Los Angeles</h3>
-						<span>832 Jobs</span>
-					</div>
-				</a>
+		</div>
+	</div>
+</div>
+<!-- Features Cities / End -->
+
+<!-- Highest Rated Freelancers -->
+<div class="section padding-top-20 padding-bottom-50 full-width-carousel-fix">
+	<div class="container">
+		<div class="row">
+
+			<div class="col-xl-12">
+				<!-- Section Headline -->   
+				<div class="section-headline margin-top-0 margin-bottom-25">
+					<h3>Les jobbeurs les plus recrutés</h3>
+					<a href="freelancers-grid-layout.html" class="headline-link">Afficher tout les jobbeurs</a>
+				</div>
 			</div>
 
-			<div class="col-xl-3 col-md-6">
-				<!-- Photo Box -->
-				<a href="jobs-list-layout-2.html" class="photo-box" data-background-image="images/featured-city-04.jpg">
-					<div class="photo-box-content">
-						<h3>Miami</h3>
-						<span>513 Jobs</span>
+			<div class="col-xl-12">
+				<div class="default-slick-carousel freelancers-container freelancers-grid-layout">
+
+					<!--Freelancer -->
+					<div class="freelancer">
+
+						<!-- Overview -->
+						<div class="freelancer-overview">
+							<div class="freelancer-overview-inner">
+								
+								<!-- Bookmark Icon -->
+								<span class="bookmark-icon"></span>
+								
+								<!-- Avatar -->
+								<div class="freelancer-avatar">
+									<div class="verified-badge"></div>
+									<a href="single-freelancer-profile.html"><img src="images/user-avatar-big-01.jpg" alt=""></a>
+								</div>
+
+								<!-- Name -->
+								<div class="freelancer-name">
+									<h4><a href="single-freelancer-profile.html">Tom Smith <img class="flag" src="images/flags/gb.svg" alt="" title="United Kingdom" data-tippy-placement="top"></a></h4>
+									<span>UI/UX Designer</span>
+								</div>
+
+								<!-- Rating -->
+								<div class="freelancer-rating">
+									<div class="star-rating" data-rating="5.0"></div>
+								</div>
+							</div>
+						</div>
+						
+						<!-- Details -->
+						<div class="freelancer-details">
+							<div class="freelancer-details-list">
+								<ul>
+									<li>Location <strong><i class="icon-material-outline-location-on"></i> London</strong></li>
+									<li>Rate <strong>$60 / hr</strong></li>
+									<li>Job Success <strong>95%</strong></li>
+								</ul>
+							</div>
+							<a href="single-freelancer-profile.html" class="button button-sliding-icon ripple-effect">View Profile <i class="icon-material-outline-arrow-right-alt"></i></a>
+						</div>
 					</div>
-				</a>
+					<!-- Freelancer / End -->
+
+					<!--Freelancer -->
+					<div class="freelancer">
+
+						<!-- Overview -->
+						<div class="freelancer-overview">
+							<div class="freelancer-overview-inner">
+								
+								<!-- Bookmark Icon -->
+								<span class="bookmark-icon"></span>
+								
+								<!-- Avatar -->
+								<div class="freelancer-avatar">
+									<div class="verified-badge"></div>
+									<a href="single-freelancer-profile.html"><img src="images/user-avatar-big-02.jpg" alt=""></a>
+								</div>
+
+								<!-- Name -->
+								<div class="freelancer-name">
+									<h4><a href="#">David Peterson <img class="flag" src="images/flags/de.svg" alt="" title="Germany" data-tippy-placement="top"></a></h4>
+									<span>iOS Expert + Node Dev</span>
+								</div>
+
+								<!-- Rating -->
+								<div class="freelancer-rating">
+									<div class="star-rating" data-rating="5.0"></div>
+								</div>
+							</div>
+						</div>
+						
+						<!-- Details -->
+						<div class="freelancer-details">
+							<div class="freelancer-details-list">
+								<ul>
+									<li>Location <strong><i class="icon-material-outline-location-on"></i> Berlin</strong></li>
+									<li>Rate <strong>$40 / hr</strong></li>
+									<li>Job Success <strong>88%</strong></li>
+								</ul>
+							</div>
+							<a href="single-freelancer-profile.html" class="button button-sliding-icon ripple-effect">View Profile <i class="icon-material-outline-arrow-right-alt"></i></a>
+						</div>
+					</div>
+					<!-- Freelancer / End -->
+
+					<!--Freelancer -->
+					<div class="freelancer">
+
+						<!-- Overview -->
+						<div class="freelancer-overview">
+							<div class="freelancer-overview-inner">
+								<!-- Bookmark Icon -->
+								<span class="bookmark-icon"></span>
+								
+								<!-- Avatar -->
+								<div class="freelancer-avatar">
+									<a href="single-freelancer-profile.html"><img src="images/user-avatar-placeholder.png" alt=""></a>
+								</div>
+
+								<!-- Name -->
+								<div class="freelancer-name">
+									<h4><a href="#">Marcin Kowalski <img class="flag" src="images/flags/pl.svg" alt="" title="Poland" data-tippy-placement="top"></a></h4>
+									<span>Front-End Developer</span>
+								</div>
+
+								<!-- Rating -->
+								<div class="freelancer-rating">
+									<div class="star-rating" data-rating="4.9"></div>
+								</div>
+							</div>
+						</div>
+						
+						<!-- Details -->
+						<div class="freelancer-details">
+							<div class="freelancer-details-list">
+								<ul>
+									<li>Location <strong><i class="icon-material-outline-location-on"></i> Warsaw</strong></li>
+									<li>Rate <strong>$50 / hr</strong></li>
+									<li>Job Success <strong>100%</strong></li>
+								</ul>
+							</div>
+							<a href="single-freelancer-profile.html" class="button button-sliding-icon ripple-effect">View Profile <i class="icon-material-outline-arrow-right-alt"></i></a>
+						</div>
+					</div>
+					<!-- Freelancer / End -->
+
+					<!--Freelancer -->
+					<div class="freelancer">
+
+						<!-- Overview -->
+						<div class="freelancer-overview">
+								<div class="freelancer-overview-inner">
+								<!-- Bookmark Icon -->
+								<span class="bookmark-icon"></span>
+								
+								<!-- Avatar -->
+								<div class="freelancer-avatar">
+									<div class="verified-badge"></div>
+									<a href="single-freelancer-profile.html"><img src="images/user-avatar-big-03.jpg" alt=""></a>
+								</div>
+
+								<!-- Name -->
+								<div class="freelancer-name">
+									<h4><a href="#">Sindy Forest <img class="flag" src="images/flags/au.svg" alt="" title="Australia" data-tippy-placement="top"></a></h4>
+									<span>Magento Certified Developer</span>
+								</div>
+
+								<!-- Rating -->
+								<div class="freelancer-rating">
+									<div class="star-rating" data-rating="5.0"></div>
+								</div>
+							</div>
+						</div>
+						
+						<!-- Details -->
+						<div class="freelancer-details">
+							<div class="freelancer-details-list">
+								<ul>
+									<li>Location <strong><i class="icon-material-outline-location-on"></i> Brisbane</strong></li>
+									<li>Rate <strong>$70 / hr</strong></li>
+									<li>Job Success <strong>100%</strong></li>
+								</ul>
+							</div>
+							<a href="single-freelancer-profile.html" class="button button-sliding-icon ripple-effect">View Profile <i class="icon-material-outline-arrow-right-alt"></i></a>
+						</div>
+					</div>
+					<!-- Freelancer / End -->
+					
+					<!--Freelancer -->
+					<div class="freelancer">
+
+						<!-- Overview -->
+						<div class="freelancer-overview">
+								<div class="freelancer-overview-inner">
+								<!-- Bookmark Icon -->
+								<span class="bookmark-icon"></span>
+								
+								<!-- Avatar -->
+								<div class="freelancer-avatar">
+									<a href="single-freelancer-profile.html"><img src="images/user-avatar-placeholder.png" alt=""></a>
+								</div>
+
+								<!-- Name -->
+								<div class="freelancer-name">
+									<h4><a href="#">Sebastiano Piccio <img class="flag" src="images/flags/it.svg" alt="" title="Italy" data-tippy-placement="top"></a></h4>
+									<span>Laravel Dev</span>
+								</div>
+
+								<!-- Rating -->
+								<div class="freelancer-rating">
+									<div class="star-rating" data-rating="4.5"></div>
+								</div>
+							</div>
+						</div>
+						
+						<!-- Details -->
+						<div class="freelancer-details">
+							<div class="freelancer-details-list">
+								<ul>
+									<li>Location <strong><i class="icon-material-outline-location-on"></i> Milan</strong></li>
+									<li>Rate <strong>$80 / hr</strong></li>
+									<li>Job Success <strong>89%</strong></li>
+								</ul>
+							</div>
+							<a href="single-freelancer-profile.html" class="button button-sliding-icon ripple-effect">View Profile <i class="icon-material-outline-arrow-right-alt"></i></a>
+						</div>
+					</div>
+					<!-- Freelancer / End -->
+								
+					<!--Freelancer -->
+					<div class="freelancer">
+
+						<!-- Overview -->
+						<div class="freelancer-overview">
+								<div class="freelancer-overview-inner">
+								<!-- Bookmark Icon -->
+								<span class="bookmark-icon"></span>
+								
+								<!-- Avatar -->
+								<div class="freelancer-avatar">
+									<a href="single-freelancer-profile.html"><img src="images/user-avatar-placeholder.png" alt=""></a>
+								</div>
+
+								<!-- Name -->
+								<div class="freelancer-name">
+									<h4><a href="#">Gabriel Lagueux <img class="flag" src="images/flags/fr.svg" alt="" title="France" data-tippy-placement="top"></a></h4>
+									<span>WordPress Expert</span>
+								</div>
+
+								<!-- Rating -->
+								<div class="freelancer-rating">
+									<div class="star-rating" data-rating="5.0"></div>
+								</div>
+							</div>
+						</div>
+						
+						<!-- Details -->
+						<div class="freelancer-details">
+							<div class="freelancer-details-list">
+								<ul>
+									<li>Location <strong><i class="icon-material-outline-location-on"></i> Paris</strong></li>
+									<li>Rate <strong>$50 / hr</strong></li>
+									<li>Job Success <strong>100%</strong></li>
+								</ul>
+							</div>
+							<a href="single-freelancer-profile.html" class="button button-sliding-icon ripple-effect">View Profile <i class="icon-material-outline-arrow-right-alt"></i></a>
+						</div>
+					</div>
+					<!-- Freelancer / End -->
+
+
+				</div>
 			</div>
 
 		</div>
 	</div>
 </div>
-<!-- Features Cities / End -->
+<!-- Highest Rated Freelancers / End-->
 
 <?php $content = ob_get_clean(); ?>
 
